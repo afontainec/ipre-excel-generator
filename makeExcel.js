ef9a1f4f05d47f62cf1c43117b65e4c02a4bc95d
Antonio Fontaine <afontaine@uc.cl>
2018-01-03 13:42:13 -0300

    add blurIndex prefix

--- makeExcel.js
+++ makeExcel.js
@@ -9,11 +9,11 @@ const output = 'auto';
 
 const workbook = excelbuilder.createWorkbook('./', 'batch_demo.xlsx');
 
-const numCol = 11;
+const numCol = 12;
 
 
 function addHeading(s) {
-  const titles = ['Compute', 'Image', 'Degradation', 'ParDeg1', 'ParDeg2', 'ParDeg3', 'Restoration', 'ParRes1', 'ParRes2', 'Descriptor', 'Output'];
+  const titles = ['Compute', 'Image', 'Prefix', 'Degradation', 'ParDeg1', 'ParDeg2', 'ParDeg3', 'Restoration', 'ParRes1', 'ParRes2', 'Descriptor', 'Output'];
   for (let i = 0; i < titles.length; i++) {
     s.set(i + 1, 1, titles[i]);
   }
@@ -25,7 +25,7 @@ function getParam(min, max, totalIterations, currentIteration) {
 }
 
 
-function addRows(sheet, degParams, degradation, images) {
+function addRows(sheet, degParams, degradation, images, prefix) {
   let index = 0;
   for (let k = 0; k < degParams[0].iteration; k++) {
     const degParam1 = getParam(degParams[0].min, degParams[0].max, degParams[0].iteration, k);
@@ -34,18 +34,21 @@ function addRows(sheet, degParams, degradation, images) {
       for (let l = 0; l < degParams[2].iteration; l++) {
         const degParam3 = getParam(degParams[2].min, degParams[2].max, degParams[2].iteration, l);
         for (let i = 0; i < images.length; i++) {
-          sheet.set(1, index + 2, 1); // Compute
+          let s = 1;
+          sheet.set(s++, index + 2, 1); // Compute
           sheet.numberFormat(1, index + 2, 1);
-          sheet.set(2, index + 2, images[i]); // image
-          sheet.set(3, index + 2, degradation); // degradation
-          sheet.set(4, index + 2, degParam1); // degpar1
-          sheet.set(5, index + 2, degParam2); // parDeg2
-          sheet.set(6, index + 2, degParam3); // parDeg3
-          sheet.set(7, index + 2, restoration); // Restoration
-          sheet.set(8, index + 2, restorationParams[0]); // parRes1
-          sheet.set(9, index + 2, restorationParams[1]); // parRes2
-          sheet.set(10, index + 2, descriptor); // Descriptor
-          sheet.set(11, index + 2, output); // Output
+          sheet.set(s++, index + 2, images[i]); // image
+
+          sheet.set(s++, index + 2, prefix); // prefix
+          sheet.set(s++, index + 2, degradation); // degradation
+          sheet.set(s++, index + 2, degParam1); // degpar1
+          sheet.set(s++, index + 2, degParam2); // parDeg2
+          sheet.set(s++, index + 2, degParam3); // parDeg3
+          sheet.set(s++, index + 2, restoration); // Restoration
+          sheet.set(s++, index + 2, restorationParams[0]); // parRes1
+          sheet.set(s++, index + 2, restorationParams[1]); // parRes2
+          sheet.set(s++, index + 2, descriptor); // Descriptor
+          sheet.set(s++, index + 2, output); // Output
           index++;
         }
       }
@@ -53,18 +56,19 @@ function addRows(sheet, degParams, degradation, images) {
   }
 }
 
-const images = [1, 2, 3, 4, 5, 6, 7];
-// const images = [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66];
+// const images = [1, 2, 3, 4, 5, 6, 7];
+const images = [51, 52, 53, 54, 55];
 
-const degradation = 'scn';
+const prefix = 'blurred';
+const degradation = 'reg';
 const degParams = [{
-  min: 5,
-  max: 20,
-  iteration: 16,
+  min: 3,
+  max: 23,
+  iteration: 11,
 }, {
-  min: 0,
-  max: 0,
-  iteration: 1,
+  min: 1,
+  max: 20,
+  iteration: 6,
 }, {
   min: 0,
   max: 0,
@@ -82,7 +86,7 @@ const sheet = workbook.createSheet('Sheet1', numCol, numRow);
 
 addHeading(sheet);
 
-addRows(sheet, degParams, degradation, images);
+addRows(sheet, degParams, degradation, images, prefix);
 
 
 // Save it
